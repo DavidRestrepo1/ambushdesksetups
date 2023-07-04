0beed5db501aa68bf6d045abd44556569a0b1362
David Restrepo <131318542+DavidRestrepo1@users.noreply.github.com>
2023-07-04 09:34:09 -0400

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "50px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
